b7d66cc803602c08fcafcee4c5026a9b746e8cfc
Hélio Mesquita <mesquitahelio@hotmail.com>
2017-01-31 11:45:16 -0200

    xcode project

---
+++ desafioLeitorQRCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  desafioLeitorQRCode
+//
+//  Created by Hélio Mesquita on 31/01/17.
+//  Copyright © 2017 Hélio Mesquita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
