1f6f45d3fa1773edc8e2e7f2f2ed0770a2c698d0
Hélio Mesquita <mesquitahelio@hotmail.com>
2017-01-31 16:41:17 -0200

    QRCode add tabela -> melhorar função de verificação se ha ou não o item na tabela

--- desafioLeitorQRCode/ExtensionPedido+DataSource+Delegate.swift
+++ desafioLeitorQRCode/ExtensionPedido+DataSource+Delegate.swift
@@ -16,14 +16,14 @@ import UIKit
         }
         
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return self.pedido!.contador
+            return self.produtos.count
         }
         
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "CelulaProduto", for: indexPath) as! CelulaProduto
             
-            cell.descricao.text = self.pedido?.produtos?[indexPath.row].descricao
+            cell.descricao.text = self.produtos[indexPath.row].descricao
             
             return cell
         }
@@ -34,7 +34,7 @@ import UIKit
         
         func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
             if editingStyle == .delete {
-                self.pedido?.produtos?.remove(at: indexPath.row)
+                self.produtos.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .top)
             } else if editingStyle == .insert {
                 // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
--- desafioLeitorQRCode/PedidoViewController.swift
+++ desafioLeitorQRCode/PedidoViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class PedidoViewController: UIViewController {
-
-    
-    var produtos = [Produto]()
-    var pedido:Pedido?
     
     let produto1 = Produto(codigoProduto: "01", codigoID: "01", descricao: "Panetone", preco: 10.0, quantidade: 100)
     let produto2 = Produto(codigoProduto: "02", codigoID: "02", descricao: "Cookie", preco: 3, quantidade: 1000)
     let produto3 = Produto(codigoProduto: "03", codigoID: "03", descricao: "Bolacha", preco: 4, quantidade: 100)
     let produto4 = Produto(codigoProduto: "04", codigoID: "04", descricao: "Brownie", preco: 2, quantidade: 1000)
     
+    var produtos = [Produto]()
     
+    var pedido:Pedido?
     
     
     @IBOutlet weak var tabela: UITableView!
@@ -29,10 +27,10 @@ class PedidoViewController: UIViewController {
         tabela.delegate = self
         tabela.dataSource = self
         
-        produtos.append(produto1)
-        produtos.append(produto2)
-        produtos.append(produto3)
-        produtos.append(produto4)
+        //produtos.append(produto1)
+        //produtos.append(produto2)
+        //produtos.append(produto3)
+        //produtos.append(produto4)
         
         var pedidoInicializado = Pedido(produto: produtos)
         pedido = pedidoInicializado
@@ -61,21 +59,76 @@ extension PedidoViewController: AdicionaProdutosTabela{
         print("Produto \(id)")
         
         switch id {
+        
         case "01":
-            self.produtos.append(self.produto1)
-            tabela.reloadData()
+            
+            if self.produtos.contains(where: { (Produto) -> Bool in
+                if Produto.codigoProduto == produto1.codigoProduto{
+                    return true
+                    // se sim nao adiciona no if que esta em baixo
+                } else {
+                    
+                    return false
+                }
+            }) == false{
+                self.produtos.append(self.produto1)
+                tabela.reloadData()
+            }
+            
             break
         case "02":
-            self.produtos.append(self.produto2)
-            tabela.reloadData()
+            if self.produtos.contains(where: { (Produto) -> Bool in
+                if Produto.codigoProduto == produto2.codigoProduto{
+                    return true
+                    // se sim nao adiciona no if que esta em baixo
+                } else {
+                    
+                    return false
+                }
+            }) == false{
+                self.produtos.append(self.produto2)
+                tabela.reloadData()
+            }
             break
+            
+            
         case "03":
-            self.produtos.append(self.produto3)
-            tabela.reloadData()
+            if self.produtos.contains(where: { (Produto) -> Bool in
+                if Produto.codigoProduto == produto3.codigoProduto{
+                    return true
+                    // se sim nao adiciona no if que esta em baixo
+                } else {
+                    
+                    return false
+                }
+            }) == false{
+                self.produtos.append(self.produto3)
+                tabela.reloadData()
+            }
             break
         case "04":
-            self.produtos.append(self.produto4)
-            tabela.reloadData()
+            
+           // esta funcao funciona verificando se ha algum produto com codigo igual
+            // ele percorre o array de produtps e retorna true caso encontre
+            // se nao tiver vai retornar falso
+            // quando retorna falso entra no segundo if
+            // na qual adiciona o produto na tabela
+            
+            // ## deve ser possivel melhora este tipo de funcao ##
+            
+           if self.produtos.contains(where: { (Produto) -> Bool in
+                if Produto.codigoProduto == produto4.codigoProduto{
+                    return true
+                    // se sim nao adiciona no if que esta em baixo
+                } else {
+                    
+                    return false
+                }
+           }) == false{
+                self.produtos.append(self.produto4)
+                tabela.reloadData()
+           }
+
             break
         default:
             break
